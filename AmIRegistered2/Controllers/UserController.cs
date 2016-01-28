5e13186261b7f60288603d864382cd8f10fe14fc
Odhran Hardy <O.hardy@kainos.com>
2016-01-28 11:45:26 +0000

    Created initial form

---
+++ AmIRegistered2/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using AmIRegistered2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AmIRegistered2.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //POST: User
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public string Search(User userModel)
+        {
+            // Check the submitted data
+            return "Here";
+            // If a match is found
+
+            // Else no match was found
+        }
+    }
+}
---
+++ AmIRegistered2/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AmIRegistered2.Models
+{
+    public class User
+    {
+        [Required]
+        [MaxLength(32)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(32)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [MaxLength(128)]
+        public string Address { get; set; }
+        [Required]
+        [Display(Name="Telephone No.")]
+        public string PhoneNo { get; set; }
+        [Required]
+        [Display(Name="National Insurance Number")]
+        [RegularExpression(@"[a-zA-Z]{2}\d{6}[a-zA-Z]{1}", ErrorMessage = "Your National Insurance No. should have the formt AA000000A")]
+        public string NINO { get; set; }
+    }
+}
